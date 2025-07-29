392853634b15a2f08b7f5ce27b1926d1d984c01f
CarmelK <carmel.kishner@gmail.com>
2025-07-29 18:39:06 +0300

    compllexity check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,17 @@
-class User < ActiveRecord::Base
-	has_many :microposts
+def translate1(force: false, locale: nil)
+	return false unless should_translate? || force
 
-	def translate1(force: false, locale: nil)
-      return false unless should_translate? || force
+	enqueue_at = translation_delay.from_now
 
-      enqueue_at = translation_delay.from_now
+	if locale.present?
+	Resque.enqueue_at(enqueue_at, Background::AI::Translate, self.class.name, id, locale.to_s)
 
-      if locale.present?
-        Resque.enqueue_at(enqueue_at, Background::AI::Translate, self.class.name, id, locale.to_s)
-
-        return true
-      end
-
-      (I18n.available_locales - I18n.default_locale).each do |language|
-        Resque.enqueue_at(enqueue_at, Background::AI::Translate, self.class.name, id, language.to_s)
-      end
-
-      true
-    end
-
-	def translate2(force: false, locale: nil)
-		return false unless should_translate? || force
-
-		I18n.available_locales.each do |language|
-    		next if language == I18n.default_locale
-    		next if locale.present? && language != locale.to_sym
-   
-  		  	Resque.enqueue_at(enqueue_at, Background::AI::Translate, self.class.name, id, language.to_s)
-		end
+	return true
 	end
+
+	(I18n.available_locales - I18n.default_locale).each do |language|
+	Resque.enqueue_at(enqueue_at, Background::AI::Translate, self.class.name, id, language.to_s)
+	end
+
+	true
 end
